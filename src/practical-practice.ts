2a25f5a9800d83760b2962f6d5e4f80794d9f0a8
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-16 15:07:09 +0530

    Challenge on Union and Literal Type

--- src/practical-practice.ts
+++ src/practical-practice.ts
@@ -10,3 +10,13 @@ for (let book of books) {
 }
 
 foundBook?.length
+
+// Challenge: Union Type and Literal type
+
+let discount: number | string = '20%'
+discount = 20
+// discount = false
+
+let orderStatus: 'processing' | 'shipped' | 'delivered' = 'processing'
+orderStatus = 'delivered'
+// orderStatus = 'reached'
